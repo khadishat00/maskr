ebe71826ae2d611a7cde2c48b613bbb4db0e64e5
khadishat00 <s163876@ap.be>
2026-05-04 15:56:39 +0200

    proces.ts updated

--- maskr-backend/routes/protected.ts
+++ maskr-backend/routes/protected.ts
@@ -1,94 +1,91 @@
 import express, { Router } from "express";
-import { secureMiddleware } from "../secureMiddleware";
-import { editUser } from "../database";
+import { createUser, login } from "../database";
+import { avatars } from "../assets";
+
+import { User } from "../types";
 
 const router: Router = express.Router();
 
-// 👇 avatars
-const avatars = [
-  "/assets/avatar01.png",
-  "/assets/avatar02.png",
-  "/assets/avatar03.png",
-  "/assets/avatar04.png",
-  "/assets/avatar05.png",
-  "/assets/avatar06.png",
-  "/assets/avatar07.png",
-  "/assets/avatar08.png",
-  "/assets/avatar09.png",
-  "/assets/avatar10.png",
-  "/assets/avatar11.png",
-  "/assets/avatar12.png",
-];
-
-// HOME
-router.get("/home", secureMiddleware, (req, res) => {
-  res.render("home", { user: req.session.user });
+router.get("/", (request, response) => {
+  response.render("index");
 });
 
-// PROFIEL
-router.get("/profiel", secureMiddleware, (req, res) => {
-  res.render("profiel", { user: req.session.user });
+router.get("/signin", (request, response) => {
+  response.render("signin", { error: "" });
 });
 
-// EDIT PAGE
-router.get("/profiel/edit", secureMiddleware, (req, res) => {
-  res.render("profiel_edit", {
-    user: req.session.user,
-    avatars,
-  });
-});
+router.post("/signin", async (request, response) => {
+  const username: string = request.body.username;
+  const email: string = request.body.email;
+  const password: string = request.body.password;
+  const image: string = avatars[0];
+  let error = "";
 
-// SAVE EDIT
-router.post("/profiel/edit", secureMiddleware, async (req, res) => {
-  const user = req.session.user;
-
-  if (!user) {
-    return res.redirect("/login");
+  //checks
+  if (username == "") {
+    error = "Username mag niet leeg zijn";
+    return response.render("signin", { error: error });
   }
 
-  const id = user._id;
+  if (email == "") {
+    error = "Email mag niet leeg zijn";
+    return response.render("signin", { error: error });
+  }
 
-  const email = req.body.email;
-  const image = req.body.image;
-  const username = req.body.username;
-  const oudeWachtwoord = req.body.oudeWachtwoord;
-  const nieuweWachtwoord = req.body.nieuweWachtwoord;
+  if (!email.includes("@")) {
+    error = "Vul een geldige email adres in";
+    return response.render("signin", { error: error });
+  }
 
-  console.log("gekozen avatar:", image);
-
-  const { success, passwordChanged } = await editUser(
-    id,
-    email,
-    image,
-    username,
-    oudeWachtwoord,
-    nieuweWachtwoord,
-  );
-
-  if (success && passwordChanged) {
-    req.session.destroy(() => {
-      res.render("login", {
-        error: "Wachtwoord gewijzigd, log opnieuw in",
-      });
+  if (password == "") {
+    error = "Wachtwoord mag niet leeg zijn";
+    return response.render("signin", { error: error });
+  }
+  try {
+    await createUser(username, email, password, image);
+    return response.render("login", {
+      error: "account is aangemaakt nu kan je inloggen",
     });
-    return;
+  } catch (e) {
+    console.log(e);
+    return response.render("signin", { error: "Probeer het opnieuw" });
+  }
+});
+
+router.get("/login", (request, response) => {
+  response.render("login", { error: "" });
+});
+
+router.post("/login", async (request, response) => {
+  const email: string = request.body.email;
+  const password: string = request.body.password;
+
+  try {
+    let user: User = await login(email, password);
+    console.log(user);
+    //session
+    delete user.password;
+    request.session.user = user;
+
+    request.session.save((err) => {
+      if (err) {
+        console.log("Session save error:", err);
+        return response.render("login", { error: "Probeer het opnieuw" });
+      }
+      return response.redirect("/home");
+    });
+  } catch (e) {
+    console.log(e);
+    return response.render("login", { error: "Ongeldige email of wachtwoord" });
   }
 
-  if (success) {
-    req.session.user = {
-      ...user,
-      username,
-      email,
-      image: image || user.image || "/assets/avatar01.png",
-    };
+  //response.render("login", { error: error });
+});
 
-    return res.redirect("/profiel");
-  }
-
-  return res.render("profiel_edit", {
-    user,
-    avatars,
+router.post("/logout", async (request, response) => {
+  request.session.destroy(() => {
+    response.redirect("/login");
   });
 });
 
-module.exports = router;
+export default router;
